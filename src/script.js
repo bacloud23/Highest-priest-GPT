1c1b440e438bff75cc62165bac7167d406fedf25
lirena <sakshamkushwaha12776@gmail.com>
2024-10-12 00:27:20 +0530

    feat: improved ui

--- src/script.js
+++ src/script.js
@@ -46,7 +46,13 @@ joinBtn.addEventListener("click", () => {
 
   // Generate and display shareable link for questioner
   const shareableLink = `${window.location.origin}?channel=${currentChannel}&role=ques`;
-  shareLink.innerHTML = `Share this link with the questioner: <br> <a href="${shareableLink}">${shareableLink}</a>`;
+  shareLink.innerHTML = `
+  <div>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5"><path d="M10.046 14c-1.506-1.512-1.37-4.1.303-5.779l4.848-4.866c1.673-1.68 4.25-1.816 5.757-.305s1.37 4.1-.303 5.78l-2.424 2.433"/><path d="M13.954 10c1.506 1.512 1.37 4.1-.303 5.779l-2.424 2.433l-2.424 2.433c-1.673 1.68-4.25 1.816-5.757.305s-1.37-4.1.303-5.78l2.424-2.433" opacity=".5"/></g></svg>
+   Share this link with the questioner: 
+  </div>
+  <br> 
+  <a class="shlink" href="${shareableLink}">${shareableLink}</a>`;
 
   // Notify the channel that the responder has joined
   pubnub.publish({
